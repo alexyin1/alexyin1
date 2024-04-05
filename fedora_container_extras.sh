f119aa99afe9bb8532549153985f51603e2a1715
Alex Yin <alexyin@ibm.com>
2024-04-05 13:40:44 -0700

    update

--- fedora_container_extras.sh
+++ fedora_container_extras.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo dnf install -y zsh htop vim tmux man
+sudo dnf install -y zsh htop vim tmux man iputils
 # justfile
 curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to "$HOME"
