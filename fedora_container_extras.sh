f65c72d82c50cf47a9b8df41feab18a19a2473b0
Alex Yin <alexyin@ibm.com>
2024-04-09 21:51:14 +0000

    add oh-my-zsh

--- fedora_container_extras.sh
+++ fedora_container_extras.sh
@@ -2,3 +2,5 @@
 sudo dnf install -y zsh htop vim tmux man iputils
 # justfile
 curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to "$HOME"
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
